58cbc0cc136f70887e006fbd328b74ffa0fc9eda
Wiffor <Wiffors@gmail.com>
2026-03-03 16:59:02 +0300

    Added new negative tests (email, name already declared in base)

--- src/test/java/api/bugred/tests/RegistrationTest.java
+++ src/test/java/api/bugred/tests/RegistrationTest.java
@@ -3,18 +3,22 @@ package api.bugred.tests;
 import api.bugred.client.RegisterClient;
 import api.bugred.model.FullUserResponse;
 import api.bugred.model.RegisterUser;
+import api.bugred.testdata.UserTestDataFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import core.BaseTest;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class RegistrationTest extends BaseTest {
 
     private final RegisterClient registerClient = new RegisterClient();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
 
     @ParameterizedTest
     @ValueSource(strings = {
@@ -27,8 +31,7 @@ public class RegistrationTest extends BaseTest {
     """})
     void doRegister_shouldReturnSuccessAndBody(String json) throws Exception
     {
-        ObjectMapper mapper = new ObjectMapper();
-        RegisterUser expectedUser = mapper.readValue(json, RegisterUser.class);
+        RegisterUser expectedUser = objectMapper.readValue(json, RegisterUser.class);
 
        FullUserResponse userResponse = registerClient.doRegister(json);
 
@@ -38,7 +41,55 @@ public class RegistrationTest extends BaseTest {
                .comparingOnlyFields("email", "name")
                .isEqualTo(expectedUser);
         assertThat(userResponse).extracting("password", "avatar").doesNotContainNull();
-
     }
 
+    @Test
+    void doRegister_shouldReturnSuccessErrorTypeAndEmailMessage() throws Exception {
+        RegisterUser registerUser = UserTestDataFactory.getUniqueUser();
+        String json = objectMapper.writeValueAsString(registerUser);
+
+        registerClient.doRegisterRaw(json); //preparation
+
+        String response = registerClient.doRegisterRaw(json)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .getBody()
+                .asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        String type = jsonPath.getString("type");
+        String message = jsonPath.getString("message");
+
+        assertThat(type).isEqualTo("error");
+        assertThat(message).contains(registerUser.getEmail());
+    }
+
+    @Test
+    void doRegister_shouldReturnSuccessErrorTypeAndNameMessage() throws Exception {
+        RegisterUser registerUser = UserTestDataFactory.getUniqueUser();
+        String prepJson = objectMapper.writeValueAsString(registerUser);
+
+        registerClient.doRegisterRaw(prepJson); //preparation
+
+        RegisterUser duplicateNameUser = UserTestDataFactory.getUserWithName(registerUser.getName());
+
+        String json = objectMapper.writeValueAsString(duplicateNameUser);
+
+        String response = registerClient.doRegisterRaw(json)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .getBody()
+                .asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        String type = jsonPath.getString("type");
+        String message = jsonPath.getString("message");
+
+        assertThat(type).isEqualTo("error");
+        assertThat(message).contains(duplicateNameUser.getName());
+    }
 }
