f987553b6ce7cf0db67a9b6411c2dc9bbb990819
Wiffor <Wiffors@gmail.com>
2026-03-03 14:28:55 +0300

    Created bugred request and responce DTO

---
+++ src/test/java/api/bugred/model/FullUserResponse.java
@@ -0,0 +1,51 @@
+package api.bugred.model;
+
+import java.time.OffsetDateTime;
+
+public class FullUserResponse extends RegisterUser{
+    private String avatar;
+    private Integer birthday;
+    private String hobby;
+    private String gender;
+    private Integer dateStart;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Integer birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Integer dateStart) {
+        this.dateStart = dateStart;
+    }
+}
