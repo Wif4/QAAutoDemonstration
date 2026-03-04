63c27b4c984909fcf382e119532e4c04594f218b
Wiffor <Wiffors@gmail.com>
2026-03-04 15:34:38 +0300

    delete unused import

--- src/test/java/api/jsonplaceholder/tests/JsonPlaceHolderTests.java
+++ src/test/java/api/jsonplaceholder/tests/JsonPlaceHolderTests.java
@@ -2,7 +2,6 @@ package api.jsonplaceholder.tests;
 
 import api.jsonplaceholder.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import core.BaseTest;
